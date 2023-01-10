894ab306b2285fed4414449c09d6173764a581be
ankrisac <ankrisac@gmail.com>
2023-01-10 16:37:59 +0000

    setup Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("hello winit")
+        .build(&event_loop)
+        .expect("winit: unable to create window");
+
+    event_loop.run(move |event, _, flow| match event {
+        Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
+            WindowEvent::Resized(new_inner_size) => {},
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {},
+            WindowEvent::CloseRequested => *flow = ControlFlow::Exit,
+            _ => {}
+        },
+        Event::RedrawRequested(_) => {},
+        Event::RedrawEventsCleared => window.request_redraw(),
+        _ => {}
+    });
 }
